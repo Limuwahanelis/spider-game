5117060ee0af15f686cae4402048b97e1122450f
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-05-03 00:50:18 +0200

    Refactor ChainCCDIKConstraintJob

--- Spider game/Spider Game/Assets/Scripts/CustomAnimationRiggingConstraints/ChainCCDIKConstraintJob.cs
+++ Spider game/Spider Game/Assets/Scripts/CustomAnimationRiggingConstraints/ChainCCDIKConstraintJob.cs
@@ -18,13 +18,15 @@ public struct ChainCCDIKConstraintJob : IWeightedAnimationJob
     public NativeArray<ReadWriteTransformHandle> chain;
 
     /// <summary>The Transform handle for the target Transform.</summary>
-    public ReadWriteTransformHandle target;
+    public ReadOnlyTransformHandle target;
 
     /// <summary> Tip of the chain which is used to determine distance from target </summary>
     public ReadWriteTransformHandle endEffector;
 
     public NativeArray<Quaternion> chainRotations;
 
+    public BoolProperty needsNewTarget;
+    public int aa;
     /// <summary>CacheIndex to ChainIK tolerance value.</summary>
     /// <seealso cref="AnimationJobCache"/>
     public UnityEngine.Animations.Rigging.CacheIndex toleranceIdx;
@@ -40,58 +42,22 @@ public struct ChainCCDIKConstraintJob : IWeightedAnimationJob
         float w = jobWeight.Get(stream);
         if (w > 0f)
         {
-            //for(int j=0;j<iteran)
-            //Debug.Log($"Axis in job {jointsAxis[0]}");
-            //for (int i = 0; i < chain.Length; ++i)
-            //
-            var handle2 = endEffector;
-            Vector3 aa = handle2.GetPosition(stream);
-            endEffector = handle2;
-            Debug.Log("bef " + aa);
-            Debug.Log("bef h" + handle2.GetPosition(stream));
             for(int i=0;i<chain.Length;i++)
             {
                 chain[i].SetRotation(stream, chainRotations[i]);
             }
-            //}
-            CCDIKSolver.SolveCCDIKFrame(ref stream, ref chain, target,ref endEffector, ref jointsAxis, ref jointsCurrentAngle, jointsMinAngle, jointsMaxAngle, index);
-            ReadWriteTransformHandle handle = chain[0];
-            //CCDIKSolver.RotateAroundAxis(ref stream, ref handle, angle.Get(stream), jointsAxis[0], endEffector);
-            //CCDIKSolver.Test2(ref stream, ref chain, angles.Get(stream), jointsAxis);
-            //CCDIKSolver.SolveCCDIK(ref stream, cache.GetRaw(toleranceIdx), (int)cache.GetRaw(maxIterationsIdx), ref chain, target, chain[chain.Length-1] ,ref jointsAxis, ref jointsCurrentAngle, jointsMinAngle, jointsMaxAngle);
-            //index--;
-            //if (index < 0) index = chain.Length - 2;
-
-
-            //Quaternion rot = Quaternion.AngleAxis(angle.Get(stream), jointsAxis[0]);
-            //Quaternion myRot = chain[0].GetRotation(stream);
-            //chain[0].SetRotation(stream, chain[0].GetRotation(stream) * Quaternion.Inverse(myRot) * rot * myRot);
-            //NativeArray<ReadWriteTransformHandle> sub = chain.GetSubArray(2, 1);
-            //CCDIKSolver.Test(ref stream, cache.GetRaw(toleranceIdx), (int)cache.GetRaw(maxIterationsIdx), ref chain, target, ref jointsAxis, ref jointsCurrentAngle, jointsMinAngle, jointsMaxAngle, index, chain[chain.Length - 1]);
-            //Debug.Log(chain[3].GetPosition(stream));
-            //chain[]
-            
-            Debug.Log($"{jointsCurrentAngle[0]} {jointsCurrentAngle[1]} {jointsCurrentAngle[2]} {jointsCurrentAngle[3]}");
-            index--;
+            if (needsNewTarget.Get(stream)) return;
+            if (!CCDIKSolver.SolveCCDIK(ref stream, cache.GetRaw(toleranceIdx), (int)cache.GetRaw(maxIterationsIdx), ref chain, target, endEffector, jointsAxis, ref jointsCurrentAngle, jointsMinAngle, jointsMaxAngle)) needsNewTarget.Set(stream, true);
             for(int i=0;i<chainRotations.Length;i++)
             {
                 chainRotations[i] = chain[i].GetRotation(stream);
             }
-            if (index < 0) index = chain.Length - 1;
-            for (int i = 0; i < chain.Length ; i++)
-            {
-
-                Debug.DrawLine(chain[i].GetPosition(stream), chain[i].GetPosition(stream) - jointsAxis[i] * 2, Color.cyan);
-            }
-            //Debug.Log(endEffector.GetPosition(stream));
-            //endEffector.SetPosition(stream, endEffector.GetPosition(stream));
-            //Debug.DrawLine(chain[0].GetPosition(stream), chain[0].GetPosition(stream) + (chain[chain.Length - 1].GetPosition(stream)-chain[0].GetPosition(stream)  ), Color.red);
         }
         else 
         {
             for (int i = 0; i < chain.Length; ++i)
                 AnimationRuntimeUtils.PassThrough(stream, chain[i]);
-            AnimationRuntimeUtils.PassThrough(stream, target);
+           // AnimationRuntimeUtils.PassThrough(stream, target);
         }
     }
 
@@ -131,9 +97,10 @@ public class ChainCCDIKConstraintJobBinder<T> : AnimationJobBinder<ChainCCDIKCon
         job.jointsMaxAngle = new NativeArray<float>(chain.Length-1, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
         job.jointsMinAngle = new NativeArray<float>(chain.Length-1, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
         job.jointsCurrentAngle = new NativeArray<float>(chain.Length, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
-        job.target = ReadWriteTransformHandle.Bind(animator, data.Target);
+        job.target = ReadOnlyTransformHandle.Bind(animator, data.Target);
         job.endEffector = ReadWriteTransformHandle.Bind(animator, chain[chain.Length-1]);
         job.chainRotations =new NativeArray<Quaternion>(chain.Length-1,Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
+        job.needsNewTarget = BoolProperty.Bind(animator, component, "m_Data.update");
         for (int i = 0; i < job.chain.Length; i++)
         {
             MyHingeJoint joint = chain[i].GetComponent<MyHingeJoint>();
@@ -145,6 +112,7 @@ public class ChainCCDIKConstraintJobBinder<T> : AnimationJobBinder<ChainCCDIKCon
             job.chainRotations[i] = joint.transform.rotation;
         }
         job.index = 0;
+        job.aa = 0;
         var cacheBuilder = new AnimationJobCacheBuilder();
         job.maxIterationsIdx = cacheBuilder.Add(data.MaxIterations);
         job.toleranceIdx = cacheBuilder.Add(data.Tolerance);
